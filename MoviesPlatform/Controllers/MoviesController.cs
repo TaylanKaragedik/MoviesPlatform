cc42460b6a7930225e8692d083932c6164f58e9e
Taylan Karagedik <45319079+TaylanKaragedik@users.noreply.github.com>
2023-08-07 01:24:21 +0300

    Proje dosyası ekle.

---
+++ MoviesPlatform/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesPlatform.Models;
+using MoviesPlatform.ViewModels;
+
+namespace MoviesPlatform.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customer = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MoviesPlatform/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesPlatform.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
